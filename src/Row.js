918677015fe7017fc69ea2d606e49adedf2f2195
Joel-Adving <joel.adving@gmail.com>
2021-04-26 18:38:32 +0200

    prod

--- src/Row.js
+++ src/Row.js
@@ -7,13 +7,13 @@ import movieTrailer from 'movie-trailer'
 const base_url = 'https://image.tmdb.org/t/p/original/'
 
 function Row({ title, fetchUrl, isLargeRow }) {
-  const [movies, setMovies] = useState(['a', 'b'])
+  const [movies, setMovies] = useState([])
   const [trailerUrl, setTrailerUrl] = useState('')
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl)
-      setMovies(request.data.results)
+      // setMovies(request.data.results)
       return request
     }
     fetchData()
