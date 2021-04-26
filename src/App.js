d8effbf12c96f3d1c415d911b183920a68fa54d6
Joel-Adving <joel.adving@gmail.com>
2021-04-26 17:22:34 +0200

    prod

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import React from "react";
-import "./App.css";
-import Row from "./Row";
-import requests from "./requests";
-import "./index.css";
-import Banner from "./Banner";
-import Nav from "./Nav";
+import React from 'react'
+import './App.css'
+import Layout from './Layout'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="app">
-      <Nav />
-      <Banner />
-      <Row
-        title="NETFLIX ORIGINALS"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
-    </div>
-  );
+    <Router basename="/index.html">
+      <Switch>
+        <Route path="/" exact component={Layout} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
